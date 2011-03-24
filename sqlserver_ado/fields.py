25d7ef182900cbee8ea0042a1a9a5feec91c5d91
mmanfre <devnull@localhost>
2011-03-24 10:37:17 -0400

    Remove sqlserver_ado.fields.BigIntegerField in favor of django.db.models.BigIntegerField.

--- sqlserver_ado/fields.py
+++ sqlserver_ado/fields.py
@@ -1,7 +1,13 @@
 """This module provides SQL Server specific fields for Django models."""
-from django.db.models import AutoField, ForeignKey, IntegerField
+from django.db.models import AutoField, ForeignKey, BigIntegerField
 from django.forms import ValidationError
 from django.utils.translation import ugettext_lazy as _
+
+__all__ = (
+    'BigAutoField',
+    'BigForeignKey',
+    'BigIntegerField',
+)
 
 class BigAutoField(AutoField):
     """A bigint IDENTITY field"""
@@ -26,22 +32,3 @@ class BigForeignKey(ForeignKey):
     """A ForeignKey field that points to a BigAutoField or BigIntegerField"""
     def db_type(self):
         return BigIntegerField().db_type()
-
-class BigIntegerField(IntegerField):
-    """A BigInteger field, until Django ticket #399 lands (if ever.)"""
-    def get_internal_type(self):
-        return "BigIntegerField"
-
-    def to_python(self, value):
-        if value is None:
-            return value
-        try:
-            return long(value)
-        except (TypeError, ValueError):
-            raise ValidationError(
-                _("This value must be a long."))
-
-    def get_db_prep_value(self, value):
-        if value is None:
-            return None
-        return long(value)
