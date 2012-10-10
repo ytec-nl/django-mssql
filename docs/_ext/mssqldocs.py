030ff96dde29f9e448d1820b1151ff0467f6adfa
Michael Manfre <michael.manfre@src.org>
2012-10-10 10:37:05 -0400

    Added docs helpers

---
+++ docs/_ext/mssqldocs.py
@@ -0,0 +1,44 @@
+import urllib
+from docutils import nodes, utils
+from sphinx import addnodes, roles, __version__ as sphinx_ver
+
+# setup function to register the extension
+def setup(app):
+    app.add_config_value('django_ticket_base_url', 'https://code.djangoproject.com/ticket/', 'env')
+    app.add_role('djangoticket', make_django_ticket_link)
+
+    app.add_config_value('mssql_ticket_base_url', 'https://code.djangoproject.com/ticket/', 'env')
+    app.add_role('mssqlticket', make_mssql_ticket_link)
+
+    app.add_crossref_type(
+        directivename = "setting",
+        rolename      = "setting",
+        indextemplate = "pair: %s; setting",
+    )
+
+def make_ticket_link(type_, base_url, name, rawtext, text, lineno, inliner, options={}, content=[]):
+    try:
+        issue_num = int(text)
+        if issue_num < 1:
+            raise ValueError
+    except ValueError:
+        msg = inliner.reporter.error(
+            '{0} issue number must be a number greater than or equal to 1; "{1}" is invalid.'.format(
+                type_, text, line=lineno
+            )
+        )
+        prb = inline.problematic(rawtext, rawtext, msg)
+        return [prb], [msg]
+    ref = base_url.rstrip('/') + '/' + urllib.quote(text, safe='')
+    node = nodes.reference(rawtext, 'Django #' + utils.unescape(text), refuri=ref, **options)
+    return [node],[]
+
+def make_django_ticket_link(name, rawtext, text, lineno, inliner, options={}, content=[]):
+    env = inliner.document.settings.env
+    base_url =  env.config.django_ticket_base_url
+    return make_ticket_link('Django', base_url, name, rawtext, text, lineno, inliner, options, content)
+
+def make_mssql_ticket_link(name, rawtext, text, lineno, inliner, options={}, content=[]):
+    env = inliner.document.settings.env
+    base_url =  env.config.mssql_ticket_base_url
+    return make_ticket_link('django-mssql', base_url, name, rawtext, text, lineno, inliner, options, content)
--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,7 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "_ext")))
 sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration -----------------------------------------------------
@@ -25,7 +26,8 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = []
+extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode',
+    'mssqldocs',]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
