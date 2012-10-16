55016c8a608cb413fede27feb963e2970eb749a2
Michael Manfre <michael.manfre@src.org>
2012-10-16 13:19:49 -0400

    supports_date_lookup_using_string = False to disable unit test

--- sqlserver_ado/base.py
+++ sqlserver_ado/base.py
@@ -22,6 +22,9 @@ class DatabaseFeatures(BaseDatabaseFeatures):
     can_return_id_from_insert = True
     
     supports_regex_backreferencing = False
+    
+    # Disable test modeltests.lookup.tests.LookupTests.test_lookup_date_as_str
+    supports_date_lookup_using_string = False
 
 # IP Address recognizer taken from:
 # http://mail.python.org/pipermail/python-list/2006-March/375505.html
