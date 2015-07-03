eb380d34326ebb4cab8a01e39a94de4d414b67dc
Michael Manfre <mmanfre@gmail.com>
2015-07-02 21:38:51 -0400

    Fixed django.db.backends.util deprecation warning

--- sqlserver_ado/patches.py
+++ sqlserver_ado/patches.py
@@ -4,7 +4,7 @@ import sys
 if sys.version_info >= (2, 7):
     # Django Ticket #17671 - Allow using a cursor as a ContextManager
     # in Python 2.7
-    from django.db.backends.util import CursorWrapper
+    from django.db.backends.utils import CursorWrapper
     if not hasattr(CursorWrapper, '__enter__'):
         enter = lambda self: self
         exit = lambda self, type, value, traceback: self.cursor.__exit__(type, value, traceback)
