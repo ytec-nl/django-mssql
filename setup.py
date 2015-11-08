ef4ea0b51db09c1ba50c53fceed48d2de6834404
Tim Graham <timograham@gmail.com>
2015-11-08 09:56:53 -0500

    Added Django 1.8 trove classifier.

--- setup.py
+++ setup.py
@@ -121,6 +121,7 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Framework :: Django',
+        'Framework :: Django :: 1.8',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
