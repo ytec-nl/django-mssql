99aa3561bc3284f556d41c24ee08794db1b3722c
Tim Graham <timograham@gmail.com>
2015-11-08 10:19:13 -0500

    Removed Python 3.3 trove classifier.

--- setup.py
+++ setup.py
@@ -124,7 +124,6 @@ setup(
         'Framework :: Django :: 1.8',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Topic :: Internet :: WWW/HTTP',
     ],
